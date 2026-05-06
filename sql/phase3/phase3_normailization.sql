fbbbef245698987ba7b3799b196b4dd7eecee98e
Ipule Pipi <ipulepipi60@gmail.com>
2026-05-06 18:19:37 -0400

    added command and query results for Orders and OrderDetails

--- sql/phase3/phase3_normailization.sql
+++ sql/phase3/phase3_normailization.sql
@@ -28,4 +28,35 @@ SELECT DISTINCT
     discontinued
 FROM raw_data;
 
+CREATE TABLE Orders AS
+SELECT DISTINCT
+    order_id,
+    customer_id,
+    employee_id,
+    order_date,
+    required_date,
+    shipped_date,
+    ship_via,
+    freight
+FROM raw_data;
+
+-- =====================================================
+-- Create OrderDetails table
+-- One row represents one product inside one order
+-- =====================================================
+
+DROP TABLE IF EXISTS OrderDetails;
+
+
+CREATE TABLE OrderDetails AS
+SELECT DISTINCT
+    order_id,
+    product_id,
+    order_unit_price,
+    quantity,
+    discount
+FROM raw_data;
+
+
+
 
